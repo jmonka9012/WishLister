779f1517f77e8b690cc845419940160b0457401d
Jacek <jmonka@edu.cdv.pl>
2026-02-07 23:11:43 +0100

    Add icon and splash screen

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart'; // Potrzebne do ograniczenia wpisywania tylko cyfr
-import 'package:wishlister/welcome_screen.dart';
+import 'welcome_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
